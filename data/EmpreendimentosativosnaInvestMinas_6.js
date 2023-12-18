c252f08c61c401b6d653230e06cceaf7f30dbe9f
davysondemmer <115158317+davysondemmer@users.noreply.github.com>
2023-12-18 14:13:15 -0300

    Add files via upload

---
+++ data/EmpreendimentosativosnaInvestMinas_6.js
@@ -0,0 +1,10 @@
+var json_EmpreendimentosativosnaInvestMinas_6 = {
+"type": "FeatureCollection",
+"name": "EmpreendimentosativosnaInvestMinas_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Latitude": -16.8375, "Longitude": -42.06417, "Empresa": "Bulbe Energia", "Setor": "Energia solar fotovoltaica", "Status": "Em operação" }, "geometry": { "type": "Point", "coordinates": [ -42.064166666666665, -16.8375 ] } },
+{ "type": "Feature", "properties": { "Latitude": -16.84861, "Longitude": -42.06389, "Empresa": "CBL", "Setor": "Mineração", "Status": "Em operação" }, "geometry": { "type": "Point", "coordinates": [ -42.063888888888883, -16.848611111111111 ] } },
+{ "type": "Feature", "properties": { "Latitude": -16.84861, "Longitude": -42.07389, "Empresa": "Distribuidora Sorvete Amigo", "Setor": "Alimentos", "Status": "Formalizado" }, "geometry": { "type": "Point", "coordinates": [ -42.063888888888883, -16.848611111111111 ] } }
+]
+}
